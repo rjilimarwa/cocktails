20951c702f38c4dd17247e94771113cde412a256
rjilimarwadjerba@gmail.com <admin@exemple.com>
2021-01-06 22:36:46 +0000

    add listsCocktails

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className="section about-section">
+        <h1 className="section-title">Qui sommes-nous?</h1>
+        <p>LuxeHotels est une entreprise hôtelière moderne et haut de gamme qui a pour passion de « créer des moments », les petits gestes pouvant faire une grande différence pour nos clients, gérants et collaborateurs. Nous insufflons de l'extraordinaire dans l'ordinaire, une philosophie qui a défini la réussite de notre marque depuis les tout débuts.
+
+        Nous sommes une enseigne internationale aux racines suisses avec une tradition de l'hôtellerie et de la restauration remontant aux années 1940, et notre vision du service est toujours restée la même : celui-ci doit être chaleureux, intuitif, personnel et unique, en un mot, « Mövenpick ». Nous cherchons également à innover pour répondre aux besoins en constante évolution des clients et à perpétuer notre riche patrimoine culinaire en créant des tendances mais sans jamais compromettre la qualité et l'authenticité. En tant qu'entreprise internationale à l'empreinte croissante, nous adoptons</p>
+    </section>
+)
+}
+
+export default About
